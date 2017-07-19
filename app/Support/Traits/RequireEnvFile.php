a8a3a40f39906372439ab746045eaf1a74217ebf
Jeff Kilroy <jeffkilroy@gmail.com>
2017-07-19 08:28:43 -0400

    Show full path of file in error

--- app/Support/Traits/RequireEnvFile.php
+++ app/Support/Traits/RequireEnvFile.php
@@ -6,7 +6,7 @@ trait RequireEnvFile{
 
     private function hasDotEnvFile(){
         if($this->config->hasDotEnvFile()){
-            $this->outputInterface->writeln('<error>No .env file found. Run "homeboy setup"</error>');
+            $this->outputInterface->writeln('<error>('.$this->config->dotEnvFilePath().') Not found. Run "homeboy setup"</error>');
             die();
         }
     }
