066621c227afcebfcb5d8d992ce29242ecaaa7c3
Jeff Kilroy <jeffkilroy@gmail.com>
2017-07-19 08:30:33 -0400

    Oops, condition needs to be reversed

--- app/Support/Traits/RequireEnvFile.php
+++ app/Support/Traits/RequireEnvFile.php
@@ -5,7 +5,7 @@ namespace App\Support\Traits;
 trait RequireEnvFile{
 
     private function hasDotEnvFile(){
-        if($this->config->hasDotEnvFile()){
+        if(!$this->config->hasDotEnvFile()){
             $this->outputInterface->writeln('<error>('.$this->config->dotEnvFilePath().') Not found. Run "homeboy setup"</error>');
             die();
         }
